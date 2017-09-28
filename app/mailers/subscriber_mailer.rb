3d49aa121634dbaf71413f39e3cf03e52a3e06de
Sir-Turtle <tolgaakkaya@live.ca>
2017-09-27 22:39:07 -0400

    Method to send subscribers a notification when a new blog post has been published.

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,2 +1,9 @@
 class SubscriberMailer < ApplicationMailer
+	default from: 'notifications@tolgaakkaya.com'
+ 
+	def new_post_notification(user)
+    	@user = user
+    	@url  = 'http://tolgaakkaya.com/'
+    	mail(to: @user.email, subject: 'New Post by Tolga')
+	end
 end
