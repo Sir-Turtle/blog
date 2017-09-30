ea2f90af09e383b14d2b3f9a94e7c893faea4ad4
Sir-Turtle <tolgaakkaya@live.ca>
2017-09-29 23:59:30 -0400

    Method to send new subscribers a confirmation email.

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,6 +1,12 @@
 class SubscriberMailer < ApplicationMailer
 	default from: 'notifications@tolgaakkaya.com'
  
+	def new_subscriber_email(user)
+    	@user = user
+    	@url  = 'http://tolgaakkaya.com/'
+    	mail(to: @user.email, subject: 'Subcribed to Tolga')
+	end
+
 	def new_post_notification(user)
     	@user = user
     	@url  = 'http://tolgaakkaya.com/'
